75da38029aa622988e47550def8af1cbe5248ecb
skuchibh <suryavamsi.gitamuniv@gmail.com>
2017-05-12 18:15:26 +0530

    REMOVED UNNEEDED TEXT

---
+++ com/surya/Main.java
@@ -0,0 +1,18 @@
+package com.surya;
+
+import com.surya.LinkedLists.LinkedList;
+
+/**
+ * Created by skuchibh on 4/26/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> llist = new LinkedList<>();
+        for (int i = 0; i <= 8; i++) {
+            llist.addLast(i + 20);
+        }
+        llist.print();llist.addLastWithNext(29,llist.getNodeAtIndex(0).next);
+        System.out.println(llist.hasCycle());
+
+    }
+}
